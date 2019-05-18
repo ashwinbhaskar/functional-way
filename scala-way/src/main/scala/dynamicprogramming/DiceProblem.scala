c4969fa73a3aa626a7ac1b9713a602d6807b29c0
Ashwin Bhaskar <ashwin.bhaskar@go-jek.com>
2019-05-18 15:26:12 +0530

    [Scala-Way] Added an example for the Rolling Dice problem

--- scala-way/src/main/scala/dynamicprogramming/DiceProblem.scala
+++ scala-way/src/main/scala/dynamicprogramming/DiceProblem.scala
@@ -2,10 +2,16 @@ package dynamicprogramming
 
 /*
 Given a number 'n', n>=0, find out the number of ways the number 'n' can be reached
-by rolling a 6-faced dice
+by rolling a 6-faced dice.
+Example : The number 3 can be reached by rolling a 3, rolling a 2 and 1, rolling 1 and 2,
+rolling a 1 three times.
+
+So, the number of ways = 4
  */
 
 object DiceProblem extends App {
+
+  println(noOfWays(3))
 
   def noOfWays(n : Int) : BigInt = n match {
     case 0 => 0
