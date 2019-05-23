be92bc48353b83560c7e7fd0f918d6d68b59ee4b
Gabriele Rastello <grastello@localhost.localdomain>
2019-05-23 22:30:07 +0200

    [Haskell-Way] add selection sort

--- haskell-way/test/Test.hs
+++ haskell-way/test/Test.hs
@@ -3,6 +3,8 @@ module Main where
 -- import algorithms
 import GravitySort
 import MergeSort
+import SelectionSort
+
 -- utility modules
 import System.Exit
 import Test.Util
@@ -20,6 +22,11 @@ testMergeSort :: Ord a => [a] -> Bool
 testMergeSort x = and [isSorted x', areSame x x']
   where x' = mergeSort x
 
+-- test selection sort on a given list
+testSelectionSort :: Ord a => [a] -> Bool
+testSelectionSort x = and [isSorted x', areSame x x']
+  where x' = selectionSort x
+
 main :: IO ()
 main = if and allTests then exitSuccess else exitFailure
 
@@ -27,6 +34,7 @@ allTests :: [Bool]
 allTests =  [
     testGravitySort testList
   , testMergeSort testList
+  , testSelectionSort testList
   ]
 
   
