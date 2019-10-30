70439f15d0c9d4c432d180fc4a3baef52db0fe72
Ashwin Bhaskar <ashwin.bhaskar@go-jek.com>
2019-10-30 15:36:38 +0530

    [Scala-Way] Fixes #20 Idomatic way of implementing quicksort

--- scala-way/src/main/scala/divideandconquer/QuickSort.scala
+++ scala-way/src/main/scala/divideandconquer/QuickSort.scala
@@ -1,8 +1,5 @@
 package divideandconquer
 
-import scala.annotation.tailrec
-import util.Utility.swap
-
 /*
 An implementation of the quick sort algorithm
  */
@@ -17,28 +14,9 @@ object QuickSort extends App{
     case Array() | Array(_) => arr
     case _ =>
       val pivotPos = arr.length/2
-      val (newArray, finalPivotPos) = movePivotToCorrectPos(arr, pivotPos)
-      quickSort(newArray.take(finalPivotPos)) ++ Array(newArray(finalPivotPos)) ++ quickSort(newArray.drop(finalPivotPos + 1))
-  }
+      val pivot = arr(pivotPos)
 
-  def movePivotToCorrectPos(array: Array[Int], pivotPos: Int): (Array[Int], Int) = {
-    val pivot = array(pivotPos)
-    val lowIndex = 1
-    val highIndex = array.length - 1
-
-    @tailrec
-    def recur(arr: Array[Int], lI: Int, hI: Int): (Array[Int], Int) =
-      if(lI > hI) (arr, hI)
-      else if (arr(lI) > pivot) {
-        val newArray = swap(arr, lI, hI)
-        recur(newArray, lI, hI - 1)
-      } else {
-        recur(arr, lI + 1, hI)
-      }
-
-    val mTemp = swap(array, 0, pivotPos)                   //move pivot to 0 index
-    val (newArray, correctPivotPos) = recur(mTemp, lowIndex, highIndex)
-    val finalArray = swap(newArray, 0, correctPivotPos)    //move pivot to correct pos
-    (finalArray, correctPivotPos)
+      val withoutPivot = arr diff Array(pivot)
+      quickSort(withoutPivot.filter(_ < pivot)) ++ Array(pivot) ++ quickSort(withoutPivot.filterNot(_ < pivot))
   }
 }
