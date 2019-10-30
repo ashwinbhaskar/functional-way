5c2dc90cc02aaf3034484048158628a799d0f485
Ashwin Bhaskar <ashwin.bhaskar@go-jek.com>
2019-10-30 12:34:51 +0530

    Modify quick sort to use immutable swap

--- scala-way/src/test/scala/HeapSortProperties.scala
+++ scala-way/src/test/scala/HeapSortProperties.scala
@@ -4,7 +4,7 @@ import util.Utility
 import org.scalacheck.Prop.forAll
 
 class HeapSortProperties extends Properties("HeapSort") {
-  
+
   property("apply") = forAll { array : Array[Int] =>
     val sortedArray = HeapSort.apply(array)
     (sortedArray.length == array.length) && Utility.isSorted(sortedArray)
