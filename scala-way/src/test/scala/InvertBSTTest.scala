575aa03ec60437ccd4cddf457f79ffa7f7918db0
Brandon <bbstilson@fastmail.com>
2020-01-18 14:47:10 -0800

    [Scala-Way] Invert BST

---
+++ scala-way/src/test/scala/InvertBSTTest.scala
@@ -0,0 +1,36 @@
+import tree.{ invert, InvertNode }
+
+import org.junit.Test
+import org.junit.Assert._
+import org.junit._
+
+class InvertBSTTest {
+  @Test def fullTreeTest() = {
+    val testTree = InvertNode(
+      1,
+      Some(InvertNode(2)),
+      Some(InvertNode(
+        3,
+        Some(InvertNode(4)),
+        Some(InvertNode(5))
+      ))
+    )
+
+    val expectedTree = InvertNode(
+      1,
+      Some(InvertNode(
+        3,
+        Some(InvertNode(5)),
+        Some(InvertNode(4))
+      )),
+      Some(InvertNode(2))
+    )
+    assertTrue(invert(testTree) == expectedTree)
+  }
+
+  @Test def emptyTree() = {
+    val testTree = InvertNode(1)
+    val expectedTree = InvertNode(1)
+    assertTrue(invert(testTree) == expectedTree)
+  }
+}
