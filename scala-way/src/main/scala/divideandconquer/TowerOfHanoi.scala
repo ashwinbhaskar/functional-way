7b62f3a40e7c04b1a001af417c8c729417dceac2
Ashwin Bhaskar <ashwin.bhaskar@go-jek.com>
2020-01-29 09:57:24 +0530

    [Scala-Way] Tower of Hanoi. Use dotty opaque types

---
+++ scala-way/src/main/scala/divideandconquer/TowerOfHanoi.scala
@@ -0,0 +1,27 @@
+package divideandconquer
+
+/* 
+Description taken from haskell course CIS 194 - https://www.cis.upenn.edu/~cis194/spring13/lectures.html [Homework1]
+The Towers of Hanoi is a classic puzzle with a solution
+that can be described recursively. Disks of different sizes are stacked
+on three pegs; the goal is to get from a starting configuration with
+all disks stacked on the first peg to an ending configuration with all
+disks stacked on the last peg.
+The only rules are
+• you may only move one disk at a time, and
+• a larger disk may never be stacked on top of a smaller one
+*/
+
+opaque type Peg = String
+object Peg
+    def apply(string: String): Peg = string
+
+opaque type Move = (Peg, Peg)
+object Move
+    def apply(fromPeg: Peg, toPeg: Peg): Move = (fromPeg, toPeg)
+
+def towerOfHanoi(numberOfDiscs: Int, fromPeg: Peg, toPeg: Peg, bufferPeg: Peg): List[Move] = numberOfDiscs match
+    case 0 => List()
+    case n => towerOfHanoi(n-1, fromPeg, bufferPeg, toPeg) ++ List((fromPeg, toPeg)) ++ towerOfHanoi(n-1, bufferPeg, toPeg, fromPeg)
+
+    
---
+++ scala-way/src/test/scala/TowerOfHanoiTest.scala
@@ -0,0 +1,32 @@
+import org.junit.Test
+import org.junit.Assert._
+import org.junit._
+import divideandconquer.{towerOfHanoi, Peg, Move}
+
+class TowerOfHanoiTest {
+    @Test
+    def testAlgorithmCorrectNess1() = {
+        val (pegA, pegB, pegC) = (Peg("pegA"), Peg("pegB"), Peg("pegC"))
+        val expectedResult = List(Move(pegA, pegC), Move(pegA, pegB), Move(pegC, pegB))
+        val numberOfDiscs = 2
+        val actualResult = towerOfHanoi(numberOfDiscs, pegA, pegB, pegC)
+        assertEquals(expectedResult, actualResult)
+    }
+    @Test
+    def testAlgorithmCorrectNess2() = {
+        val (pegA, pegB, pegC) = (Peg("pegA"), Peg("pegB"), Peg("pegC"))
+        val expectedResult = List(Move(pegA, pegB), Move(pegA, pegC), Move(pegB, pegC), 
+                                    Move(pegA, pegB), Move(pegC, pegA), Move(pegC, pegB), Move(pegA, pegB))
+        val numberOfDiscs = 3
+        val actualResult = towerOfHanoi(numberOfDiscs, pegA, pegB, pegC)
+        assertEquals(expectedResult, actualResult)
+    }
+    @Test
+    def testAlgorithmCorrectNess3() = {
+        val (pegA, pegB, pegC) = (Peg("pegA"), Peg("pegB"), Peg("pegC"))
+        val expectedResult = List()
+        val numberOfDiscs = 0
+        val actualResult = towerOfHanoi(numberOfDiscs, pegA, pegB, pegC)
+        assertEquals(expectedResult, actualResult)
+    }
+}
